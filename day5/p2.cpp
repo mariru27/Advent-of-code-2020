16bc9589075fd924cdbdc6fb257fac2afa89116a
Marina Rusu <46792157+mariru27@users.noreply.github.com>
2020-12-05 08:42:26 +0200

    Create p2.cpp

---
+++ day5/p2.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include <string> 
+#include<unordered_map>
+#include<map>
+#include <algorithm>    
+#include "mynamespace.h"
+
+
+
+int main()
+{
+	std::ifstream in;
+	std::ofstream out;
+	in.open("in.txt");
+	out.open("out.txt");
+
+	std::string line;
+	int res1 = 0, res2 = 0;
+	int maxid = -1;
+	std::vector<int> seats;
+
+	while (in >> line)
+	{
+		/*std::getline(in, line);*/
+		std::string rules = line.substr(0, 7);
+		int front = 0, back = 127;
+		int mid = 0;
+		int i = 0;
+		while (i < rules.size())
+		{
+			mid = front + (back - front) / 2;
+			if (rules[i] == 'F')
+			{
+				back = mid;
+			}
+			if (rules[i] == 'B')
+			{
+				front = mid + 1;
+			}
+
+			++i;
+		}
+		if (rules[rules.size() - 1] == 'F')
+			res1 = front;
+		else
+			res1 = back;
+
+		std::string rules2 = line.substr(7, line.size() - 7);
+		int left = 0, right = 7;
+		int mid2 = 0;
+		int j = 0;
+		while (j < rules2.size())
+		{
+			mid2 = left + (right - left) / 2;
+			if (rules2[j] == 'R')
+			{
+				left = mid2 + 1;
+			}
+			if (rules2[j] == 'L')
+			{
+				right = mid2 ;
+			}
+
+			++j;
+		}
+		if (rules2[rules2.size() - 1] == 'L')
+			res2 = left;
+		else
+			res2 = right;
+
+		if (maxid < (res1 * 8) + res2)
+			maxid = (res1 * 8) + res2;
+		seats.push_back((res1 * 8) + res2);
+
+	}
+	//std::cout << res1 << " " << res2 << std::endl;
+	//std::cout << (res1 *8) + res2;
+	std::sort(seats.begin(), seats.end());
+	int j = seats[0];
+
+	//std::cout << maxid;
+	for (int i = 63; i <= 935; i++)
+	{
+		auto it = std::find(seats.begin(), seats.end(), i);
+		if (it == seats.end())
+		{
+			std::cout << i;
+			break;
+		}
+	}
+	
+	return 0;
+}
